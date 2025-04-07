9b5cecebc2d4029a614e7aec965b7eec2845913f
Pedro Henrique <phas050902@gmail.com>
2025-04-06 21:53:12 -0300

    fix: project structure

---
+++ src/main/java/com/example/auth_user_backend/AuthUserBackendApplication.java
@@ -0,0 +1,12 @@
+package com.example.auth_user_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthUserBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthUserBackendApplication.class, args);
+    }
+}
--- src/test/java/com/example/auth_user_backend/AuthUserBackendApplicationTests.java
+++ src/test/java/com/example/auth_user_backend/AuthUserBackendApplicationTests.java
@@ -3,7 +3,7 @@ package com.example.auth_user_backend;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = AuthUserBackendApplication.class)
 class AuthUserBackendApplicationTests {
 
 	@Test
